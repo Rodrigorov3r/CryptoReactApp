6e77a08fb3e20faeab575d5b877a393b6cb933ce
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-07-23 21:32:50 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <p>saca saca a esta ricula</p>
+      </>
+    </div>
+  );
+}
+
+export default App;
